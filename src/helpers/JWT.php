69161c098ec3ad13b8d15c6169a307825db15bb6
Arthur Skobara <skobara.arthur@gmail.com>
2017-07-10 12:30:12 +0700

    add isExpired method

--- src/helpers/JWT.php
+++ src/helpers/JWT.php
@@ -6,6 +6,8 @@ use Yii;
 use Exception;
 use Lcobucci\JWT\Builder as JWTBuilder;
 use Lcobucci\JWT\Signer\Hmac\Sha256 as Signer;
+use Lcobucci\JWT\Parser as JWTParser;
+use Lcobucci\JWT\ValidationData;
 use beardedandnotmuch\user\models\JWTSourceInterface;
 
 class JWT
@@ -32,5 +34,25 @@ class JWT
             ->setExpiration($now + $duration)
             ->sign(new Signer(), $user->getSecretKey())
             ->getToken();
+    }
+
+    /**
+     * undocumented function
+     *
+     * @param string $token
+     *
+     * @return bool
+     */
+    public static function isExpired($token)
+    {
+        $request = Yii::$app->getRequest();
+
+        $token = (new JWTParser())->parse((string) $token);
+
+        $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
+        $data->setIssuer($request->hostInfo);
+        $data->setAudience($request->hostInfo);
+
+        return !$token->validate($data);
     }
 }
